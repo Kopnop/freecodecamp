b879c947aa0e22f22aa9d992b29dcc8074c30b14
Kopnop <robin.raetze@web.de>
2023-03-21 10:59:27 +0100

    some testing

---
+++ frontend/my-calculator/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
