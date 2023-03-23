77a0fb2d9496dbf619c7ad5b8aa5f8d99acce681
Kopnop <robin.raetze@web.de>
2023-03-23 16:22:24 +0100

    final version of calc

--- frontend/my-calculator/src/App.js
+++ frontend/my-calculator/src/App.js
@@ -2,69 +2,19 @@ import Footer from "./Components/Footer";
 import "./App.css"
 import { useEffect, useState } from "react";
 import buttonsData from "./Data/buttons.json"
+
 function App() {
 
   const [currentValue, setCurrentValue] = useState("");
   const [topDisplay, setTopDisplay] = useState("");
-  const [bottomDisplay, setBottomDisplay] = useState("")
   const [result, setResult] = useState();
   const [currentOperator, setCurrentOperator] = useState("")
   const [lastClicked, setLastClicked] = useState("")
-  const operations = ["equals", "plus", "minus", "divide", "multi"]
+  const operations = ["equals", "add", "subtract", "divide", "multiply"]
 
   useEffect(() => {
-    console.log("lastValue changed to: " + result);
+    setTopDisplay(result ? result : "0")
   }, [result])
-
-  function handleNumber(number) {
-    if (operations.includes(lastClicked)) {
-      setCurrentValue(number)
-      setBottomDisplay(number)
-    } else {
-      const newValue = currentValue + number
-      setCurrentValue(newValue)
-      setBottomDisplay(newValue)
-    }
-    if (lastClicked === "equals") {
-      setResult()
-      setTopDisplay("")
-    }
-  }
-
-  function handleOperator() {
-    if (!result) {
-      //calc with 0
-      setResult(currentValue)
-      setTopDisplay(currentValue)
-      setCurrentValue()
-      setBottomDisplay("")
-    } else {
-      if (currentValue) {
-        //perform calc
-        let newValue
-        switch (currentOperator) {
-          case "+":
-            newValue = Number(result) + Number(currentValue)
-            break
-          case "-":
-            newValue = Number(result) - Number(currentValue)
-            break
-          case "*":
-            newValue = Number(result) * Number(currentValue)
-            break
-          case "/":
-            newValue = Number(result) / Number(currentValue)
-            break
-          default:
-          //TODO: check case
-        }
-        setResult(newValue)
-        setCurrentValue()
-        setTopDisplay(newValue)
-        setBottomDisplay()
-      }
-    }
-  }
 
   function handleClick(e) {
     switch (e.target.id) {
@@ -98,19 +48,24 @@ function App() {
       case 'nine':
         handleNumber("9")
         break;
-      case 'dot':
-        //TODO
-        setCurrentValue(prevText => prevText + ".")
+      case 'decimal':
+        if (!currentValue) {
+          setCurrentValue("0.")
+        } else {
+          if (!currentValue.includes(".")) {
+            setCurrentValue(prevText => prevText + ".")
+          }
+        }
         break;
       case 'equals':
         handleOperator()
         setCurrentOperator("=")
         break;
-      case 'plus':
+      case 'add':
         handleOperator()
         setCurrentOperator("+")
         break;
-      case 'minus':
+      case 'subtract':
         handleOperator()
         setCurrentOperator("-")
         break;
@@ -118,33 +73,35 @@ function App() {
         handleOperator()
         setCurrentOperator("/")
         break;
-      case 'multi':
+      case 'multiply':
         handleOperator()
         setCurrentOperator("*")
         break;
-      case 'inverse':
-        //TODO
-        break;
-      case 'sqr':
-        //TODO
-        break;
-      case 'sqrt':
-        //TODO
-        break;
+      // case 'inverse':
+      //   //TODO
+      //   break;
+      // case 'sqr':
+      //   //TODO
+      //   break;
+      // case 'sqrt':
+      //   //TODO
+      //   break;
       case 'negate':
-        //TODO
+        if (currentValue) { setCurrentValue(prevValue => prevValue * -1) }
         break;
       case 'back':
-        //TODO: check if correct
-        if (currentValue.length > 0) { setCurrentValue(prevText => prevText.slice(0, -1)) }
+        if (currentValue && currentValue.length > 0) {
+          if (currentValue.length === 2 && currentValue[0] === "0" && currentValue[1] === ".") {
+            setCurrentValue()
+          } else {
+            setCurrentValue(prevText => prevText.slice(0, -1))
+          }
+        }
         break;
       case 'clear':
-        //TODO: check if correct
         setCurrentValue("")
-        setTopDisplay("")
-        setResult(0)
+        setResult()
         setCurrentOperator("")
-        setBottomDisplay("")
         break;
       default:
         console.log("default");
@@ -152,16 +109,71 @@ function App() {
     setLastClicked(e.target.id)
   }
 
+  function handleNumber(number) {
+    if (operations.includes(lastClicked) || currentValue === undefined) {
+      setCurrentValue(number)
+    } else {
+      if (currentValue.length > 30) {
+        return
+      }
+      if (number === "0") {
+        if ((!currentValue || currentValue[0] === "0") && !currentValue.includes(".")) {
+          return;
+        }
+      }
+      const newValue = currentValue + number
+      setCurrentValue(newValue)
+    }
+    if (lastClicked === "equals") {
+      setResult()
+    }
+  }
+  function handleOperator() {
+    if (result === undefined) {
+      //calc with 0
+      setResult(currentValue)
+      setCurrentValue()
+    } else {
+      if (currentValue) {
+        //perform calc
+        let newValue
+        switch (currentOperator) {
+          case "+":
+            newValue = Math.round((Number(result) + Number(currentValue)) * 1e6) / 1e6
+
+            break
+          case "-":
+            newValue = Math.round((Number(result) - Number(currentValue)) * 1e6) / 1e6
+
+            break
+          case "*":
+            newValue = Math.round((Number(result) * Number(currentValue)) * 1e6) / 1e6
+            break
+          case "/":
+            if (currentValue === "0") {
+              setResult("can't divide by 0")
+              return
+            }
+            newValue = Math.round((Number(result) / Number(currentValue)) * 1e6) / 1e6
+            break
+          default:
+          //TODO: check case
+        }
+        setResult(newValue)
+        setCurrentValue()
+      }
+    }
+  }
+
   return (
     <div className="App">
       <main>
         <div className="calc">
-          <div className="display">
-            <p className="totalValueText">{topDisplay ? topDisplay : "topDisplay"}{currentOperator ? " " + currentOperator : ""}</p>
-            <p className="currentValueText">{currentValue ? currentValue : currentOperator ? currentOperator : "currentValue"}</p>
-            <p className="currentValueText">{bottomDisplay ? bottomDisplay : "bottomDisplay"}</p>
+          <div className="display" id="display">
+            <p className="topDisplay">{topDisplay}{currentOperator ? " " + currentOperator : ""}</p>
+            <p className="bottomDisplay">{currentValue ? currentValue : currentOperator ? currentOperator : "0"}</p>
           </div>
-          {buttonsData.map(item => <button key={item.name} id={item.name} className={item.placeholder} onClick={handleClick}>{item.displayName}</button>)}
+          {buttonsData.map(item => <button key={item.name} id={item.name} className={item.className} onClick={handleClick}>{item.displayName}</button>)}
         </div>
       </main>
       <Footer />
